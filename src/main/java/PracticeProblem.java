9a34d0a8f3d3208713c646632462f36f5e2d9e7e
Jim-Lii <343347795@tdsb.ca>
2026-05-29 15:10:51 +0000

    codespace created

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,14 @@
+/*
+Title: Minesweeper - Culminating Assignment
+Author: Jim Li
+Date Created: May 29, 2026
+Date Last Modified: May 29, 2026
+ */
+
+import java.util.Scanner;
+import java.util.Random;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
-}
