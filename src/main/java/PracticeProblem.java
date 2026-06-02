fa061c7d7664f79cabcf61be46d36c28375ccdb1
Jim-Lii <343347795@tdsb.ca>
2026-06-02 14:44:03 +0000

    custom width and height

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,7 @@
 Title: Minesweeper - Culminating Assignment
 Author: Jim Li
 Date Created: May 29, 2026
-Date Last Modified: Jun 1, 2026
+Date Last Modified: Jun 2, 2026
  */
 
 import java.util.Scanner;
@@ -25,26 +25,41 @@ public class PracticeProblem {
 		System.out.println("r [integer] [integer] - Reveals the tile at (x, y)");
 		System.out.println("f [integer] [integer] - Flags the tile at (x, y)\n");
 
-		//get grid size (cannot be 3x3 or smaller, or ridiculously large). Grid size is for length AND width.
-		System.out.print("Input grid size (between 4 and 40): ");
-		int gridSize = 0;
-		while (gridSize < 4 || gridSize > 40){
+		//get grid size (cannot be 3x3 or smaller, or ridiculously large). Separate width and height
+		System.out.print("Input grid width (between 4 and 40): ");
+		int width = 0;
+		while (width < 4 || width > 40){
 			while (!input.hasNextInt()){
 				if (!input.hasNextInt()){
-					System.out.print("Invalid input!\nInput grid size (between 4 and 40): ");
+					System.out.print("Invalid input!\nInput grid width (between 4 and 40): ");
 				}
 				input.nextLine();
 			}
-			gridSize = input.nextInt();
-			if (gridSize < 4 || gridSize > 40){
-				System.out.print("Invalid input!\nInput grid size (between 4 and 40): ");
+			width = input.nextInt();
+			if (width < 4 || width > 40){
+				System.out.print("Invalid input!\nInput grid width (between 4 and 40): ");
+			}
+		}
+
+		System.out.print("Input grid height (between 4 and 40): ");
+		int height = 0;
+		while (height < 4 || height > 40){
+			while (!input.hasNextInt()){
+				if (!input.hasNextInt()){
+					System.out.print("Invalid input!\nInput grid height (between 4 and 40): ");
+				}
+				input.nextLine();
+			}
+			height = input.nextInt();
+			if (height < 4 || height > 40){
+				System.out.print("Invalid input!\nInput grid height (between 4 and 40): ");
 			}
 		}
 
 		//get mine amount (must be at least 1, cannot have less than 9 empty spaces)
 		System.out.print("Input mine amount (between 1 and # of tiles, minus 9): ");
 		int mines = 0;
-		while (mines < 1 || mines > gridSize * gridSize - 9){
+		while (mines < 1 || mines > width * height - 9){
 			while (!input.hasNextInt()){
 				if (!input.hasNextInt()){
 					System.out.print("Invalid input!\nInput mine amount (between 1 and # of tiles, minus 9): ");
@@ -52,21 +67,21 @@ public class PracticeProblem {
 				input.nextLine();
 			}
 			mines = input.nextInt();
-			if (mines < 1 || mines > gridSize * gridSize - 9){
+			if (mines < 1 || mines > width * height - 9){
 				System.out.print("Invalid input!\nInput mine amount (between 1 and # of tiles, minus 9): ");
 			}
 		}
 
 		//create the display array (starts as just question marks). Only array visible to player
-		char[][] display = new char[gridSize][gridSize];
-		for (int i = 0; i < gridSize; i++){
-			for (int j = 0; j < gridSize; j++){
+		char[][] display = new char[width][height];
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
 				display[j][i] = '?';
 			}
 		}
 
 		//print display (starting board)
-		printArray(display, gridSize);
+		printArray(display, width, height);
 
 		//this array records each action/move
 		//the first digit determines the type of action: 0 = invalid input, 1 = reveal, 2 = flag
@@ -76,7 +91,7 @@ public class PracticeProblem {
 		//take first action (no flagging)
 		System.out.print("Input first action: ");
 		while (action[0] != 1){ //while invalid
-			action = processAction(display, gridSize);
+			action = processAction(display, width, height);
 			if (action[0] == 0){
 				System.out.print("Invalid Input!\nInput first action: ");
 			}
@@ -89,22 +104,22 @@ public class PracticeProblem {
 		display[col][row] = '0';
 		
 		//generate the actual board
-		char[][] board = generateBoard(gridSize, mines, col, row);
-		printArray(board, gridSize);
+		char[][] board = generateBoard(width, height, mines, col, row);
+		printArray(board, width, height);
 
 		//print starting board
-		display = clear(display, board, gridSize, col, row);
-		printArray(display, gridSize);
+		display = clear(display, board, width, height, col, row);
+		printArray(display, width, height);
 
 		//this is where the game actually begins
 		boolean loss = false; //boolean for if the player hits a mine
-		int flags = 0, tiles = gridSize * gridSize, revealedTiles = tiles - count(display, '?');
+		int flags = 0, tiles = width * height, revealedTiles = tiles - count(display, '?', width, height);
 		//game goes until a mine is hit or everything has been revealed
 		while (!loss && revealedTiles < tiles - mines){
 			System.out.println("Flags left: " + (mines - flags));
 			System.out.print("Input next action: ");
 			while (action[0] == 0 || action[0] == 3){
-				action = processAction(display, gridSize);
+				action = processAction(display, width, height);
 				if (action[0] == 0){
 					System.out.print("Invalid Input!\nInput next action: ");
 				}
@@ -121,7 +136,7 @@ public class PracticeProblem {
 				}
 				//automatic clearing
 				if (display[col][row] == '0'){ //if 0 is revealed
-					display = clear(display, board, gridSize, col, row);
+					display = clear(display, board, width, height, col, row);
 				}
 			}
 			if (action[0] == 2){ //if flag
@@ -134,35 +149,35 @@ public class PracticeProblem {
 				}
 			}
 			action[0] = 0; //reset action
-			revealedTiles = gridSize * gridSize - count(display, '?') - count(display, 'F');
-			printArray(display, gridSize);
+			revealedTiles = width * height - count(display, '?', width, height) - count(display, 'F', width, height);
+			printArray(display, width, height);
 		}
 		
 		//ending message
 		if (loss){
-			printArray(board, gridSize);
+			printArray(board, width, height);
 			System.out.println("You Lost!");
 		} else {
-			printArray(board, gridSize);
+			printArray(board, width, height);
 			System.out.println("You Win!");
 		}
 	}
 
 	//method that generates the board. Avoids spawning mines on starting area or on already placed mines
-	public static char[][] generateBoard(int gridSize, int mines, int startX, int startY){
+	public static char[][] generateBoard(int width, int height, int mines, int startX, int startY){
 		//mine generation
 		Random random = new Random();
-		char[][] board = new char[gridSize][gridSize];
+		char[][] board = new char[width][height];
 		//create the board
-		for (int i = 0; i < gridSize; i++){
-			for (int j = 0; j < gridSize; j++){
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
 				board[j][i] = '0';
 			}
 		}
 		int placedMines = 0;
 		while (placedMines < mines){ //go until mines reaches target
-			int ranX = random.nextInt(gridSize); //random position generation
-			int ranY = random.nextInt(gridSize);
+			int ranX = random.nextInt(width); //random position generation
+			int ranY = random.nextInt(height);
 			//logic that prevents mines in starting position and repeating mine spawns
 			if ((ranX < startX - 1 || ranX > startX + 1 || ranY < startY - 1 || ranY > startY + 1) && board[ranX][ranY] != 'M'){
 				board[ranX][ranY] = 'M';
@@ -171,16 +186,16 @@ public class PracticeProblem {
 		}
 
 		//number generation
-		for (int i = 0; i < gridSize; i++){
-			for (int j = 0; j < gridSize; j++){
-				board[j][i] = findMines(board, gridSize, j, i);
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
+				board[j][i] = findMines(board, width, height, j, i);
 			}
 		}
 		return board;
 	}
 
 	//method that counts surrounding mines, for board generation
-	public static char findMines(char[][] board, int gridSize, int col, int row){
+	public static char findMines(char[][] board, int width, int height, int col, int row){
 		if (board[col][row] == '0'){ //if selected tile is 0
 			char counter = '0';
 			//first if: checks if index is out of bounds
@@ -191,19 +206,19 @@ public class PracticeProblem {
 			if (col - 1 > -1 && board[col-1][row] == 'M'){ //left
 				counter++;
 			}
-			if (col - 1 > -1 && row + 1 < gridSize && board[col-1][row+1] == 'M'){ //down left
+			if (col - 1 > -1 && row + 1 < height && board[col-1][row+1] == 'M'){ //down left
 				counter++;
 			}
-			if (row + 1 < gridSize && board[col][row+1] == 'M'){ //down
+			if (row + 1 < height && board[col][row+1] == 'M'){ //down
 				counter++;
 			}
-			if (col + 1 < gridSize && row + 1 < gridSize && board[col+1][row+1] == 'M'){ //down right
+			if (col + 1 < width && row + 1 < height && board[col+1][row+1] == 'M'){ //down right
 				counter++;
 			}
-			if (col + 1 < gridSize && board[col+1][row] == 'M'){ //right
+			if (col + 1 < width && board[col+1][row] == 'M'){ //right
 				counter++;
 			}
-			if (col + 1 < gridSize && row - 1 > -1 && board[col+1][row-1] == 'M'){ //up right
+			if (col + 1 < width && row - 1 > -1 && board[col+1][row-1] == 'M'){ //up right
 				counter++;
 			}
 			if (row - 1 > -1 && board[col][row-1] == 'M'){ //up
@@ -214,11 +229,11 @@ public class PracticeProblem {
 		return 'M'; //if it's a mine
 	}
 
-	//method that counts the amount of a certain character in a character array, to count revealed tiles
-	public static int count(char[][] array, char target){
+	//method that counts the amount of a certain character in a character array, for counting revealed tiles
+	public static int count(char[][] array, char target, int width, int height){
 		int counter = 0;
-		for (int i = 0; i < array.length; i++){
-			for (int j = 0; j < array.length; j++){
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
 				if (array[j][i] == target){
 					counter++;
 				}
@@ -229,7 +244,7 @@ public class PracticeProblem {
 
 	//method that requests input then converts it into action	
 	//if input is invalid, it will return 0 at index 0
-	public static int[] processAction(char[][] display, int gridSize){
+	public static int[] processAction(char[][] display, int width, int height){
 		Scanner input = new Scanner(System.in);
 		int[] action = {0, 0, 0};
 		String actionInput = input.nextLine(); //get input
@@ -251,10 +266,10 @@ public class PracticeProblem {
 		}
 
 		//try to parse the column and row
-		if (splitInput[1].matches("[0-9]+") && Integer.parseInt(splitInput[1]) < gridSize){
+		if (splitInput[1].matches("[0-9]+") && Integer.parseInt(splitInput[1]) < width){ //col
 			action[1] = Integer.parseInt(splitInput[1]);
 		}
-		if (splitInput[2].matches("[0-9]+") && Integer.parseInt(splitInput[2]) < gridSize){
+		if (splitInput[2].matches("[0-9]+") && Integer.parseInt(splitInput[2]) < height){ //row
 			action[2] = Integer.parseInt(splitInput[2]);
 		}
 
@@ -267,7 +282,7 @@ public class PracticeProblem {
 
 	//method that automatically clears the board, so the game is more playable
 	//only runs if the revealed thing is a 0
-	public static char[][] clear(char[][] display, char[][] board, int gridSize, int startCol, int startRow){
+	public static char[][] clear(char[][] display, char[][] board, int width, int height, int startCol, int startRow){
 		Queue<int[]> queue = new ArrayDeque<>(); //queue for things to visit
 		ArrayList<int[]> visited = new ArrayList<>(); //arraylist of visited tiles
 		queue.add(new int[]{startCol, startRow});
@@ -288,31 +303,31 @@ public class PracticeProblem {
 						queue.add(new int[]{col - 1, row});
 					}
 				}
-				if (col - 1 > -1 && row + 1 < gridSize){ //down left
+				if (col - 1 > -1 && row + 1 < height){ //down left
 					display[col - 1][row + 1] = board[col - 1][row + 1];
 					if (board[col - 1][row + 1] == '0'){
 						queue.add(new int[]{col - 1, row + 1});
 					}
 				}
-				if (row + 1 < gridSize){ //down
+				if (row + 1 < height){ //down
 					display[col][row + 1] = board[col][row + 1];
 					if (board[col][row + 1] == '0'){
 						queue.add(new int[]{col, row + 1});	
 					}
 				}
-				if (col + 1 < gridSize && row + 1 < gridSize){ //down right
+				if (col + 1 < width && row + 1 < height){ //down right
 					display[col + 1][row + 1] = board[col + 1][row + 1];
 					if (board[col + 1][row + 1] == '0'){
 						queue.add(new int[]{col + 1, row + 1});
 					}
 				}
-				if (col + 1 < gridSize){ //right
+				if (col + 1 < width){ //right
 					display[col + 1][row] = board[col + 1][row];
 					if (board[col + 1][row] == '0'){
 						queue.add(new int[]{col + 1, row});
 					}
 				}
-				if (col + 1 < gridSize && row - 1 > -1){ //up right
+				if (col + 1 < width && row - 1 > -1){ //up right
 					display[col + 1][row - 1] = board[col + 1][row - 1];
 					if (board[col + 1][row - 1] == '0'){
 						queue.add(new int[]{col + 1, row - 1});
@@ -343,20 +358,20 @@ public class PracticeProblem {
 	}
 
 	//method that prints 2D arrays, for displaying field and testing. Also shows coordinates.
-	public static void printArray(char[][] array, int gridSize){
+	public static void printArray(char[][] array, int width, int height){
 		System.out.print("\n   ");
-		for (int i = 0; i < gridSize; i++){
+		for (int i = 0; i < width; i++){ //top numbers
 			System.out.print(i);
 			if (i < 10){
 				System.out.print(" ");
 			}
 		}
-		for (int i = 0; i < gridSize; i++){
+		for (int i = 0; i < height; i++){ //side numbers and tiles
 			System.out.print("\n" + i + " ");
 			if (i < 10){
 				System.out.print(" ");
 			}
-			for (int j = 0; j < gridSize; j++){
+			for (int j = 0; j < width; j++){
 				System.out.print(array[j][i] + " ");
 			}
 		}
